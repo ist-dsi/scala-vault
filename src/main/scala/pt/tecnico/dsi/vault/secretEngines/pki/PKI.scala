cf2b61d5aed217335a973036a0d50402edbf09e0
João <joao.paulo.reis@tecnico.ulisboa.pt>
2026-01-08 15:56:13 +0000

    Add decoder to get result of set issuer

--- src/main/scala/pt/tecnico/dsi/vault/secretEngines/pki/PKI.scala
+++ src/main/scala/pt/tecnico/dsi/vault/secretEngines/pki/PKI.scala
@@ -287,13 +287,13 @@ final class PKI[F[_]: Concurrent: Client](val path: String, val uri: Uri)(implic
     * @param certificatePEM Specifies the certificate in PEM format. May optionally append additional CA certificates
     *                       to populate the whole chain, which will then enable returning the full chain from issue and sign operations.
     */
-  def setSignedIntermediate(certificatePEM: String): F[Unit] = 
+  def setSignedIntermediate(certificatePEM: String): F[Issuers] = 
     execute(POST(Map("certificate" -> certificatePEM), uri / "intermediate" / "set-signed", token))
+
+  def setSignedIntermediate(certificate: X509Certificate): F[Issuers] = setSignedIntermediate(PKI.pemEncode(certificate))
 
   def setDefaultIssuer(issuer: String): F[Unit] = 
     execute(POST(Map("default" -> issuer), uri / "config" / "issuers", token))
-
-  def setSignedIntermediate(certificate: X509Certificate): F[Unit] = setSignedIntermediate(PKI.pemEncode(certificate))
 
   // We could make the result be a dependent type based upon the Type value
   // Devolve com Context:
---
+++ src/main/scala/pt/tecnico/dsi/vault/secretEngines/pki/models/Issuers.scala
@@ -0,0 +1,9 @@
+package pt.tecnico.dsi.vault.secretEngines.pki.models
+
+import io.circe.Decoder
+import io.circe.derivation.{deriveDecoder, renaming}
+
+object Issuers {
+  implicit val decoder: Decoder[Issuers] = deriveDecoder(renaming.snakeCase)
+}
+case class Issuers(importedIssuers: List[String], importedKeys: List[String])
